c9023fb1dfc955c6d3122366d1f628eee52681db
Philippe Kabore <kaborephil@gmail.com>
2021-07-08 17:08:42 -0500

    added a class travel to JS file

---
+++ scripts.js
@@ -0,0 +1,10 @@
+//create a travel class
+
+class travel {
+    constructor(destinationName, location, photo, description) {
+        this.destinationName = destinationName;
+        this.location = location;
+        this.photo = photo;
+        this.description = description;
+    }
+}
