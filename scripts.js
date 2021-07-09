0a2317b047f57ee311e9abf295b1c9a60122ef92
Philippe Kabore <kaborephil@gmail.com>
2021-07-09 13:05:15 -0500

    added an event listener to JS file

--- scripts.js
+++ scripts.js
@@ -8,3 +8,16 @@ class travel {
         this.description = description;
     }
 }
+
+const form = document.getElementById("form");
+const wishList = document.getElementById("wishlist");
+
+    form.addEventListener("submit", function(event){
+        event.preventDefault();
+    //addItems();
+    
+    const newText = document.createElement("h3");
+    newText.innerHTML ="Hello World!"
+    newText.setAttribute("id", "myh3");
+    document.getElementById("wishlist").appendChild(newText);
+});
